fab6d3124c065f88323baf2c3b088d59b8547d13
Cleyton Silva <silva.rcleyton@gmail.com>
2021-10-11 12:53:13 -0300

    List user by id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
 
   # GET /users
-  # Lista all users
+  # List all users
   def index
     @users = User.all
     render json: @users, status: :ok
+  end
+
+  # GET /users/id
+  # List user by id
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: :ok
   end
 
   # POST /users
