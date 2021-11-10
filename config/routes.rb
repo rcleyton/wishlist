275dd5c40c2263317ea898393e62b719e835f88a
Cleyton Silva <silva.rcleyton@gmail.com>
2021-11-10 09:29:27 -0300

    Versioning api v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[ show create update destroy ]
-  resources :favorites, only: %i[ show create ]
-  
+
+  namespace 'api' do 
+    namespace 'v1' do
+      resources :users, only: %i[ show create update destroy ]
+      resources :favorites, only: %i[ show create ]
+    end
+  end
+
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
+
 end
