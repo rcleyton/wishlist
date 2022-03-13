c13e8b43657f5eda5b7f699bdd5bb5b601d85688
Cleyton Silva <silva.rcleyton@gmail.com>
2022-03-13 12:27:28 -0300

    Refactor user favorite

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,9 +2,6 @@ module Api
   module V1
     class FavoritesController < ApplicationController
       before_action :authorize_request
-      before_action :user_list
-
-      include Request
 
       def show 
         @favorite = Favorite.find(params[:id])
@@ -12,32 +9,31 @@ module Api
       end
 
       def create
-        if get_product == 404
+        if product.code == 404
           render json: { error: "Product not found" }, status: :not_found
 
-        elsif @user_list.present? 
-          check_id = product_present(@user_list.product)
+        elsif @current_user.favorite.present? 
+          check_id = product_present(@current_user.favorite.product)
 
           if check_id.include?(true)
             render json: { status: "Product is already added to wishlist" }
           else
-            @user_list.product << get_product
-            @user_list.save
+            @current_user.favorite.product << product.body
+            @current_user.favorite.save
             render json: { success: "Product added to wishlist" }, status: :created
           end
 
-        elsif @user_list.nil?
-          @favorite = Favorite.create(user_id: @current_user.id,
-                                      product: [get_product])
+        elsif @current_user.favorite.nil?
+          @favorite = Favorite.create(user_id: @current_user.id, product: [product])
           @favorite.save
           render json: { success: "Product added to wishlist" }, status: :created
         end
       end
 
       private
-      def user_list
-        @user_list = @current_user.favorite
-      end
+      def product
+        HTTParty.get("#{ENV['URI_BASE']}#{params[:product_id]}/")
+      end 
 
       def product_present(product)
         product.map do |prod|
